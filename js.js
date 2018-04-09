40f21329b92e600cf4e767a422392d26ad7f3dc4
Gregoire <gregoire.meunier@hotmail.fr>
2018-04-09 14:28:43 +0200

    put a navbar on each page of the website

--- js.js
+++ js.js
@@ -3,16 +3,17 @@ var i;
 for (i = 0; i < dropdown.length; i++) 
 {
   dropdown[i].addEventListener("click", 
-  function() 
-  {
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") 
+    function() 
     {
-      dropdownContent.style.display = "none";
-    } 
-    else 
-    {
-      dropdownContent.style.display = "block";
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") 
+      {
+        dropdownContent.style.display = "none";
+      } 
+      else 
+      {
+        dropdownContent.style.display = "block";
+      }
     }
-  });
+  );
 }

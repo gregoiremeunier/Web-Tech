e325124373468c6076fb6f5bc9419adbd6ad4326
Gregoire <gregoire.meunier@hotmail.fr>
2018-04-09 20:36:24 +0200

    changement contraste function LowVision

--- js.js
+++ js.js
@@ -29,10 +29,12 @@ function LowVisionMode()
   document.body.style.fontSize = '200%';
   $(document.body).addClass("text-white");
   $("h4").removeClass("italic");
+  document.body.style.backgroundImage="url(access.jpg)";
 }
 function NormalVisionMode()
 {
   document.body.style.fontSize = '100%';
   $(document.body).removeClass("text-white")
   $("h4").addClass("italic");
+  document.body.style.backgroundImage="url(fond.jpg)";
 }

03243840d76f64a7978489d213f7ea02512a3361
Gregoire <gregoire.meunier@hotmail.fr>
2018-04-10 17:10:57 +0200

    change js.js in gene.js AND prev/next button on Clanky.js

--- js/55ziskajClanky.js
+++ js/55ziskajClanky.js
@@ -1,15 +1,7 @@
-/**
- * Created by stefan.kr
- *
- * ziskajClanky means get articles
- */
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 //Code executed after loading this script:
 
 //Number of articles per one page
-var articlesPerPage=2;
+var articlesPerPage=10;
 
 //Domain name of the server with the article database
 var server="wt.kpi.fei.tuke.sk";
@@ -21,7 +13,6 @@ writeArticles2Html(0, articlesPerPage, server, 'clanky', 'navigacia');
 
 //Functions:
 
-
 /**
  * Creates and returns HTML code for the navigation part of the page
  * @param startIndex - index of the first of the displayed articles
@@ -30,38 +21,63 @@ writeArticles2Html(0, articlesPerPage, server, 'clanky', 'navigacia');
  * @returns {string} - HTML code for the navigation part of the page
 
  */
-
 function navHtml(startIndex, articlesCount, articlesTotalCount){
     var htmlKod="";
-    if(articlesCount>0){
+    if(articlesCount>0)
+    {
         htmlKod+="<hr> ";
-        htmlKod+="Vypisujem články  "+(startIndex+1)+" až "+(startIndex+articlesCount)+" z "+ articlesTotalCount +" článkov. <br />";
         htmlKod+="(Displaying articles  "+(startIndex+1)+" to "+(startIndex+articlesCount)+" from "+ articlesTotalCount  +" articles.) <br /> <br />";
-
     }
-    htmlKod+=" <button onclick=\"writeArticles2Html("+startIndex+
-             ", articlesPerPage, server, 'clanky', 'navigacia')\">" +
-             "Načítaj znova / Reload</button>";
-    return htmlKod;
+    if((startIndex + 1 - articlesPerPage > 0 )&&(startIndex + articlesPerPage < articlesTotalCount)) //we are far from the border => we can go prev and next
+    {
+        var prev = startIndex - articlesPerPage;
+        var next = startIndex + articlesPerPage;
+       // document.write("prev1 :"+prev);
+        htmlKod+=" <button onclick=\"writeArticles2Html("+prev+", articlesPerPage, server, 'clanky', 'navigacia')\">" +"Prev</button>";
+        htmlKod+=" <button onclick=\"writeArticles2Html("+next+", articlesPerPage, server, 'clanky', 'navigacia')\">" +"Next</button>";
+        return htmlKod;
+    }
+    else if((startIndex + articlesPerPage)< articlesTotalCount) //we are far from the upper border => we can go next
+    {
+        var next = startIndex + articlesPerPage;
+        //document.write("next2 :"+next);
+        htmlKod+=" <button onclick=\"writeArticles2Html("+startIndex+", articlesPerPage, server, 'clanky', 'navigacia')\">" +"Prev</button>";
+        htmlKod+=" <button onclick=\"writeArticles2Html("+next+", articlesPerPage, server, 'clanky', 'navigacia')\">" +"Next</button>";
+        return htmlKod;
+    }
+    else if((startIndex + 1 - articlesPerPage) > 0) //we are far from the lower border => we can go prev
+    {
+        
+        var prev = startIndex - articlesPerPage;
+        //document.write("prev3 :"+prev);
+        htmlKod+=" <button onclick=\"writeArticles2Html("+prev+", articlesPerPage, server, 'clanky', 'navigacia')\">" +"Prev</button>";
+        htmlKod+=" <button onclick=\"writeArticles2Html("+startIndex+", articlesPerPage, server, 'clanky', 'navigacia')\">" +"Next</button>";
+        return htmlKod;
+    }
+    else
+    {
+        htmlKod+=" <button onclick=\"writeArticles2Html("+startIndex+", articlesPerPage, server, 'clanky', 'navigacia')\">" +"Prev</button>";
+        htmlKod+=" <button onclick=\"writeArticles2Html("+startIndex+", articlesPerPage, server, 'clanky', 'navigacia')\">" +"Next</button>";
+        return htmlKod;
+    }
 }
-
-
 
 /**
  * Creates and returns HTML code with the list of articles, obtained from the object articles
  * @param articles  - JSON object with articles
  * @returns {string} - HTML code with the list of articles
  */
-function articlesHtml(articles){
+function articlesHtml(articles)
+{
     var count;
     var htmlKod="";
-    if(count=articles.articles.length){ //ak su nejake clanky (if there are some articles)
+    if(count=articles.articles.length)
+    { //ak su nejake clanky (if there are some articles)
         for(var i=0; i<count; i++)
-            htmlKod+="<p>"+articles.articles[i].author+": "+articles.articles[i].title+" </p>";
+            htmlKod+="<p>"+articles.articles[i].author+": "+articles.articles[i].title+" was written in "+articles.articles[i].dateCreated+" </p>";
     }
     return htmlKod;
 }
-
 
 /**
  * Writes authors and names of articles to a html element
@@ -80,9 +96,6 @@ function JSON2Html(articles, articlesElmId, navElmId, startIndex, max){
     }
 }
 
-
-
-
 /**
  * Opens a dialog window with an error message
  * @param status -  value os XMLHttpRequest.status
@@ -90,8 +103,6 @@ function JSON2Html(articles, articlesElmId, navElmId, startIndex, max){
 function errorDialog(status){
     window.alert("Error when reading server data.\nStatus= "+status);
 }
-
-
 
 /**
  * Executes XMLHttpRequest GET request and processes the response in the form of an object in the JSON format.
@@ -109,7 +120,7 @@ function getJSONAllBr(url, successHandler, errorHandler){
     var xhr = typeof XMLHttpRequest != 'undefined'
         ? new XMLHttpRequest()
         : new ActiveXObject('Microsoft.XMLHTTP');
-    xhr.open('GET', url, true);
+    xhr.open('GET', url, true); // true = asynchrone; false = synchrone (attend la réponse du serveur)
     xhr.onreadystatechange = function() { //alternativne mozem pouzit (alternatively we can use) xhr.addEventListener("readystatechange",funkcia, false),
         // ale tu je pouzita anonymna funkcia a bolo by to iba neprehladnejsie (but here we use an anonymous function)
         var status;
@@ -127,8 +138,6 @@ function getJSONAllBr(url, successHandler, errorHandler){
     xhr.send();
 };
 
-
-
 /**
  * Writes article data to the element with id=articlesElmId and HTML code for the navigation part to the element with id=navElmId
  * @param startIndex - index of the first article that is displayed. Articles are indexed from 0
@@ -144,3 +153,4 @@ function writeArticles2Html(startIndex, max, server, articlesElmId, navElmId){
         function(JSONObj){JSON2Html(JSONObj, articlesElmId, navElmId, startIndex, max)},
         function(status){errorDialog(status)});
 }
+ 

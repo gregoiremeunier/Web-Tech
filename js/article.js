45a8a14a3fd1e7f3e0635ce55b4147986960025c
Gregoire <gregoire.meunier@hotmail.fr>
2018-05-05 13:25:41 +0200

    edit article WORKING

--- js/article.js
+++ js/article.js
@@ -1,8 +1,22 @@
 //Domain name of the server with the article database
 var server="wt.kpi.fei.tuke.sk";
 
+var artId = queryString2obj().id;
+var restURL ="http://"+server+"/api/article/"+artId;
+
 //Code executed when the script is loaded
 writeArticle2Html("article","comments");
+
+//Adding functionality for buttons
+document.getElementById("btArtList").addEventListener("click", function(){
+    window.location.href='DisplayListArticle.html';
+});
+document.getElementById("btUpdate").addEventListener("click", function(){
+    window.location.href='articleForm.html?id='+artId;
+});
+document.getElementById("btDelete").addEventListener("click", function(){
+    deleteArticle(restURL);
+});
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,5 +83,33 @@ function writeArticle2Html(articleElmId,comments)
                 errorDialog(textStatus+"("+errorThrown+")");
             }
         });
+    }
+}
+
+/**
+ * Deletes an article, including its comments
+ * @param articleId - id of the article to be deleted
+ */
+function deleteArticle(sourceURL)
+{
+    if(window.confirm("Do you really wish to delete the article, including its comments?"))
+    {
+        AJAXCall('DELETE', sourceURL,
+            "", null,
+            function (xhr) 
+            {
+                var status = xhr.status;
+                console.log(status + " " + xhr.statusText + " " + xhr.responseText);
+                if (status == 204) 
+                { //204 = no content
+                    window.alert("article successfully deleted.");
+                    window.location.href = "DisplayListArticle.html";
+                }
+                else
+                {
+                    errorAlert("delete failed.",xhr);
+                }
+            }
+        );
     }
 }
